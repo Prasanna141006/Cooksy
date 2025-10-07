71dadb6b19322e6c919090f72ff1dc5c9d751518
Prasanna141006 <prasannachandra.nst@gmail.com>
2025-10-07 11:56:03 +0530

    Add files via upload

---
+++ Cooksyyy/Cooksyyy/CooksyyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CooksyyyApp.swift
+//  Cooksyyy
+//
+//  Created by Nxtwave on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CooksyyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
